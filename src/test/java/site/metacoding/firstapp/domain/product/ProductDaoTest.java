41ab42d4620459c56ef07397ef05f763cd562ed5
basakk123 <basakk@naver.com>
2022-09-30 11:18:31 +0900

    ProductDao Junit 테스트 완료

--- src/test/java/site/metacoding/firstapp/domain/product/ProductDaoTest.java
+++ src/test/java/site/metacoding/firstapp/domain/product/ProductDaoTest.java
@@ -1,6 +1,9 @@
 package site.metacoding.firstapp.domain.product;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
@@ -28,5 +31,77 @@ public class ProductDaoTest {
 
         // then (검증)
         assertEquals("바나나", productPS.getProductName());
+    }
+
+    @Test
+    public void findAll_test() {
+        // given
+
+        // when
+        List<Product> productListPS = productDao.findAll();
+        System.out.println(productListPS.get(0).getProductName());
+        // then
+        assertEquals(2, productListPS.size());
+    }
+
+    // Junit은 메서드 실행직전에 트랜잭션이 걸리고, 메서드 실행이 끝나면 rollback 됨
+    // Mybatis는 Resultset을 자바 Entity로 변경해줄 때, 빈생성자를 호출하고 setter가 없어도 값을 매핑해준다.
+    @Test
+    public void insert_test() {
+        // given
+        String productName = "수박";
+        Integer productPrice = 1000;
+        Integer productQty = 100;
+        Product product = new Product(productName, productPrice, productQty);
+
+        // when
+        int result = productDao.insert(product);
+
+        // then
+        assertEquals(1, result);
+    } // rollback
+
+    @Test
+    public void update_test() {
+        // given
+        Integer productId = 1;
+        String productName = "수박";
+        Integer productPrice = 1000;
+        Integer productQty = 100;
+
+        Product product = new Product(productName, productPrice, productQty);
+        product.setProductId(productId);
+
+        // verify
+        Product productPS = productDao.findById(product.getProductId());
+        assertTrue(productPS == null ? false : true);
+
+        // when
+        // product [id=1, productName="수박", productPrice=1000, productQty=100]
+        // productPS [id=1, productName="바나나", productPrice=3000, productQty=98,
+        // createdAt=2022-09-29]
+        productPS.update(product);
+        // productPS [id=1, productName="수박", productPrice=1000, productQty=100,
+        // createdAt=2022-09-29]
+        int result = productDao.update(productPS);
+
+        // then
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void deleteById_test() {
+        // given
+        Integer productId = 1;
+
+        // verify
+        Product productPS = productDao.findById(productId);
+        assertTrue(productPS == null ? false : true);
+
+        // when
+        int result = productDao.deleteById(productId);
+
+        // then
+        assertEquals(1, result);
     }
 }
